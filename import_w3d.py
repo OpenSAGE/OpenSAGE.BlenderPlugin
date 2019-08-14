eb804a5a483fedd72d3fa3255c6a28d3a252d6c9
Michael Schnabel <michael.schnabel@exone.com>
2019-08-14 10:15:56 +0200

    refactoring, moved adaptive delta code

--- import_w3d.py
+++ import_w3d.py
@@ -6,6 +6,7 @@ import os
 from io_mesh_w3d.w3d_structs import *
 from io_mesh_w3d.w3d_io_binary import *
 from io_mesh_w3d.utils_w3d import *
+from io_mesh_w3d.w3d_adaptive_delta import * 
 
 #######################################################################################
 # Hierarchy
--- w3d_adaptive_delta.py
+++ w3d_adaptive_delta.py
@@ -1,72 +1,90 @@
-import os
-import bpy
-import bmesh
 import math
+from mathutils import Quaternion
+
+def fill_with_exponents_of_10(table):
+    for i in range(16):
+        table.append(pow(10, i - 8.0))
+
+
+def fill_with_sinus_function(table):
+    for i in range(240):
+        num = i / 240.0
+        table.append(1.0 - math.sin(90.0 * num * math.pi / 180.0))
+
 
 def calculate_table():
-    result = []
-
-    # First 16 entries are exponents of 10
-    for i in range(0,16):
-        result.append(pow(10, i - 8.0))
-
-    # Fill a precomputed sinus function
-    for i in range(0,240):
-        num = i / 240.0
-        result.append(1.0 - math.sin(90.0 * num * math.pi / 180.0))
-
-    return result
+    table = []
+    fill_with_exponents_of_10(table)
+    fill_with_sinus_function(table)
+    return table
 
 delta_table = calculate_table()
 
-def decode(data, numFrames, scale):
+
+def to_signed(byte):
+    if byte > 127:
+        return (256-byte) * (-1)
+    else:
+        return byte
+
+
+def get_deltas(block, numBits):
+    deltas = [None] * 16
+
+    for i, byte in enumerate(block.deltaBytes):
+        index = i * 2
+        if numBits == 4:
+            deltas[index] = byte
+            # Bitflip
+            if (deltas[index] & 8) != 0:
+                deltas[index] |= 0xF0
+            else:
+                deltas[index] &= 0x0F
+            deltas[index + 1] = byte >> 4
+        elif numBits == 8:
+            # Bitflip
+            if byte & 0x80 != 0:
+                byte &= 0x7F
+            else:
+                byte |= 0x80
+            deltas[i] = to_signed(byte)
+
+    return deltas
+
+    
+def decode(data, channel, scale):
     scaleFactor = 1.0
 
     if data.bitCount == 8:
         scaleFactor = 1.0 / 16.0
 
-    result = [None] * numFrames
+    result = [None] * channel.numTimeCodes
     result[0] = data.initialValue
 
     for i, deltaBlock in enumerate(data.deltaBlocks):
-        blockIndex = deltaBlock.blockIndex
-        blockScale = delta_table[blockIndex]
+        blockScale = delta_table[deltaBlock.blockIndex]
         deltaScale = blockScale * scale * scaleFactor
 
         vectorIndex = deltaBlock.vectorIndex
-
         deltas = get_deltas(deltaBlock, data.bitCount)
 
         for j, delta in enumerate(deltas):
-            index = ((i / data.vectorLength) * 16) + j + 1
-            if index >= numFrames:
+            idx = int(math.floor((i / channel.vectorLen) * 16) + j + 1)
+            if idx >= channel.numTimeCodes:
                 break
 
-            value = result[index - 1][vectorIndex] + deltaScale * delta
-            result[index] = result[index][vectorIndex] = value
-
-            index+=1
+            if channel.type == 6:
+                index = (vectorIndex + 1) % 4 # access quat as xyzw instead of wxyz
+                value = result[idx - 1][index] + deltaScale * delta
+                if (result[idx] == None):
+                    result[idx] = Quaternion()
+                    result[idx].w = result[idx - 1].w
+                    result[idx].x = result[idx - 1].x
+                    result[idx].y = result[idx - 1].y
+                    result[idx].z = result[idx - 1].z
+                result[idx][index] = value
+                
+            else:
+                result[idx] = result[idx - 1] + deltaScale * delta
 
     return result
-
-def get_deltas(block, bits):
-    deltas = [None] * 16
-
-    for i, byte in enumerate(block.deltaBytes):
-        if bits == 4:
-            # Bitflip
-            if byte & 8 != 0:
-                deltas[i*2] = byte | 0xF0
-            else:
-                deltas[i*2] = byte & 0x0F
-        elif bits == 8:
-            # Bitflip
-            if byte & 0x80 != 0:
-                deltas[i*2] = byte & 0x7F
-            else:
-                deltas[i*2] = byte | 0x80
-
-        else:
-            print("Unsupported bit count: " + bits)
-
-    return deltas
