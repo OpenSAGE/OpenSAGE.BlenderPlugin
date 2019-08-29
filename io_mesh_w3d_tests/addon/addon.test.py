2a437d08ee24177c0d45d4e79b66a6887699917e
Tarcontar <michael-schnabel@gmx.net>
2019-08-29 20:53:56 +0200

    got first test setup workin

---
+++ io_mesh_w3d_tests/addon/addon.test.py
@@ -0,0 +1,26 @@
+import unittest
+import io_mesh_w3d
+
+def runTestCases(testCases):
+    #Until a better solution for knowing if the logger's error count should be used to quit the testing,
+    #we are currently saying only 1 is allow per suite at a time (which is likely how it should be anyways)
+    assert len(testCases) == 1, "Currently, only one test case per suite is supported at a time"
+    suite = unittest.defaultTestLoader.loadTestsFromTestCase(testCases[0])
+    test_result = unittest.TextTestRunner().run(suite)
+
+    #See XPlane2Blender/tests.py for documentation. The strings must be kept in sync!
+    return_string = "RESULT: After {testsRun} tests got {errors} errors, {failures} failures, and {skipped} skipped"\
+        .format(testsRun=test_result.testsRun,
+                errors=len(test_result.errors),
+                failures=len(test_result.failures),
+                skipped=len(test_result.skipped))
+    print(return_string)
+
+
+class TestAddon(unittest.TestCase):
+    def test_addon_enabled(self):
+        self.assertIsNotNone(io_mesh_w3d.bl_info)
+
+runTestCases([TestAddon])
+
+
---
+++ runTests.bat
@@ -0,0 +1,2 @@
+python tests.py --blender "C:\Program Files\Blender Foundation\Blender\blender.exe"
+Pause
