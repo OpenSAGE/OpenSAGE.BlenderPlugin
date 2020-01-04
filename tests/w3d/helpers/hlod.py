3433737788dab0a9250896632a94df70ef6782aa
Michael Schnabel <michael-schnabel@gmx.net>
2020-01-04 19:32:23 +0100

    test refactoring, work on multiple lods

--- tests/w3d/helpers/hlod.py
+++ tests/w3d/helpers/hlod.py
@@ -2,6 +2,7 @@
 # Written by Stephan Vedder and Michael Schnabel
 
 import unittest
+import sys
 from io_mesh_w3d.w3d.structs.hlod import *
 from tests.w3d.helpers.version import *
 
@@ -10,10 +11,10 @@ from io_mesh_w3d.w3d.structs.hlod import *
 from tests.w3d.helpers.version import get_version, compare_versions
 
 
-def get_hlod_header(model_name, hierarchy_name):
+def get_hlod_header(model_name, hierarchy_name, lod_count=1):
     return HLodHeader(
         version=get_version(),
-        lod_count=1,
+        lod_count=lod_count,
         model_name=model_name,
         hierarchy_name=hierarchy_name)
 
@@ -25,15 +26,15 @@ def compare_hlod_headers(self, expected, actual):
     self.assertEqual(expected.hierarchy_name, actual.hierarchy_name)
 
 
-def get_hlod_array_header():
+def get_hlod_array_header(count=0, size=0.0):
     return HLodArrayHeader(
-        model_count=0,
-        max_screen_size=0.0)
+        model_count=count,
+        max_screen_size=size)
 
 
 def compare_hlod_array_headers(self, expected, actual):
     self.assertEqual(expected.model_count, actual.model_count)
-    self.assertEqual(expected.max_screen_size, actual.max_screen_size)
+    self.assertAlmostEqual(expected.max_screen_size, actual.max_screen_size, 2)
 
 
 def get_hlod_sub_object(bone, name):
@@ -93,6 +94,44 @@ def get_hlod_minimal(model_name="containerName", hierarchy_name="TestHierarchy")
     return HLod(
         header=get_hlod_header(model_name, hierarchy_name),
         lod_arrays=[get_hlod_array_minimal()])
+
+
+def get_hlod_4_levels(model_name="containerName", hierarchy_name="TestHierarchy"):
+    array0 = HLodArray(
+        header=get_hlod_array_header(count=3, size=0.0),
+        sub_objects=[])
+
+    array0.sub_objects = [get_hlod_sub_object(bone=0, name="containerName.mesh1"),
+                            get_hlod_sub_object(bone=0, name="containerName.mesh2"),
+                            get_hlod_sub_object(bone=0, name="containerName.mesh3")]
+
+    array1 = HLodArray(
+        header=get_hlod_array_header(count=3, size=1.0),
+        sub_objects=[])
+
+    array1.sub_objects = [get_hlod_sub_object(bone=0, name="containerName.mesh1_1"),
+                            get_hlod_sub_object(bone=0, name="containerName.mesh2_1"),
+                            get_hlod_sub_object(bone=0, name="containerName.mesh3_1")]
+
+    array2 = HLodArray(
+        header=get_hlod_array_header(count=3, size=0.3),
+        sub_objects=[])
+
+    array2.sub_objects = [get_hlod_sub_object(bone=0, name="containerName.mesh1_2"),
+                            get_hlod_sub_object(bone=0, name="containerName.mesh2_2"),
+                            get_hlod_sub_object(bone=0, name="containerName.mesh3_2")]
+
+    array3 = HLodArray(
+        header=get_hlod_array_header(count=3, size=0.03),
+        sub_objects=[])
+
+    array3.sub_objects = [get_hlod_sub_object(bone=0, name="containerName.mesh1_3"),
+                            get_hlod_sub_object(bone=0, name="containerName.mesh2_3"),
+                            get_hlod_sub_object(bone=0, name="containerName.mesh3_3")]
+
+    return HLod(
+        header=get_hlod_header(model_name, hierarchy_name, lod_count=4),
+        lod_arrays=[array0, array1, array2, array3])
 
 
 def compare_hlods(self, expected, actual):
