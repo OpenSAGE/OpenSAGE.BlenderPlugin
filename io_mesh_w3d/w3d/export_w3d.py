b548d42c94d083f705a33645538647dc44641ceb
Michael Schnabel <michael.schnabel@exone.com>
2020-01-07 16:06:19 +0100

    file can now be imported in w3d viewer

--- io_mesh_w3d/w3d/export_w3d.py
+++ io_mesh_w3d/w3d/export_w3d.py
@@ -33,6 +33,10 @@ def save(self, export_settings):
 
         sknFile = open(self.filepath, "wb")
 
+        if export_mode == 'HAM':
+            #if len(hierarchy.pivots) > 1:
+            hierarchy.write(sknFile)
+
         boxes = retrieve_boxes(hierarchy, containerName)
         for box in boxes:
             box.write(sknFile)
@@ -40,13 +44,10 @@ def save(self, export_settings):
         for mesh in meshes:
             mesh.write(sknFile)
 
-        if hlod.lod_arrays and len(hlod.lod_arrays[-1].sub_objects) > 1:
-            hlod.write(sknFile)
+        #if hlod.lod_arrays and len(hlod.lod_arrays[-1].sub_objects) > 1:
+        hlod.write(sknFile)
 
         if export_mode == 'HAM':
-            if len(hierarchy.pivots) > 1:
-                hierarchy.write(sknFile)
-
             timecoded = False
             compressionMode = export_settings['w3d_compression']
 
