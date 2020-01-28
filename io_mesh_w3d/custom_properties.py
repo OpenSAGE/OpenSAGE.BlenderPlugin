fa9be51629f0556950f66a0666e02412f3c8a3b6
Michael Schnabel <michael-schnabel@gmx.net>
2020-01-28 19:53:35 +0100

    started with custom property tests

--- io_mesh_w3d/custom_properties.py
+++ io_mesh_w3d/custom_properties.py
@@ -41,23 +41,6 @@ Object.dazzle_type = EnumProperty(
     default='DEFAULT')
 
 
-class OBJECT_PANEL_PT_w3d(Panel):
-    bl_label = 'W3D Properties'
-    bl_space_type = 'PROPERTIES'
-    bl_region_type = 'WINDOW'
-    bl_context = 'object'
-
-    def draw(self, context):
-        layout = self.layout
-        col = layout.column()
-        col.prop(context.active_object, 'object_type')
-        if context.active_object.object_type == 'DAZZLE':
-            col = layout.column()
-            col.prop(context.active_object, 'dazzle_type')
-        col = layout.column()
-        col.prop(context.active_object, 'userText')
-
-
 ##########################################################################
 # Material
 ##########################################################################
@@ -72,7 +55,7 @@ Material.attributes = EnumProperty(
         ('ARGB_EMISSIVE_ONLY', 'ArgbEmissiveOnly', 'desc: todo', 2),
         ('COPY_SPECULAR_TO_DIFFUSE', 'CopySpecularToDiffuse', 'desc: todo', 4),
         ('DEPTH_CUE_TO_ALPHA', 'DepthCueToAlpha', 'desc: todo', 8)],
-    default={'DEFAULT'},
+    default=set(),
     options={'ENUM_FLAG'})
 
 Material.surface_type = EnumProperty(
@@ -349,92 +332,6 @@ Material.tex_ani_fps_NPR_lastFrame_frameOffset_0 = FloatVectorProperty(
     description='TODO')
 
 
-class MATERIAL_PROPERTIES_PANEL_PT_w3d(Panel):
-    bl_label = 'W3D Properties'
-    bl_space_type = 'PROPERTIES'
-    bl_region_type = 'WINDOW'
-    bl_context = 'material'
-
-    def draw(self, context):
-        layout = self.layout
-        col = layout.column()
-        col.prop(context.object.active_material, 'attributes')
-        col = layout.column()
-        col.prop(context.object.active_material, 'surface_type')
-        col = layout.column()
-        col.prop(context.object.active_material, 'translucency')
-        col = layout.column()
-        col.prop(context.object.active_material, 'vm_args_0')
-        col = layout.column()
-        col.prop(context.object.active_material, 'vm_args_1')
-        col = layout.column()
-        col.prop(context.object.active_material, 'technique')
-        col = layout.column()
-        col.prop(context.object.active_material, 'ambient')
-        col = layout.column()
-        col.prop(context.object.active_material, 'opacity')
-        col = layout.column()
-        col.prop(context.object.active_material, 'alpha_test')
-        col = layout.column()
-        col.prop(context.object.active_material, 'blend_mode')
-        col = layout.column()
-        col.prop(context.object.active_material, 'bump_uv_scale')
-        col = layout.column()
-        col.prop(context.object.active_material, 'edge_fade_out')
-        col = layout.column()
-        col.prop(context.object.active_material, 'depth_write')
-        col = layout.column()
-        col.prop(context.object.active_material, 'sampler_clamp_uv_no_mip_0')
-        col = layout.column()
-        col.prop(context.object.active_material, 'sampler_clamp_uv_no_mip_1')
-        col = layout.column()
-        col.prop(context.object.active_material, 'num_textures')
-        col = layout.column()
-        col.prop(context.object.active_material, 'texture_0')
-        col = layout.column()
-        col.prop(context.object.active_material, 'texture_1')
-        col = layout.column()
-        col.prop(context.object.active_material, 'secondary_texture_blend_mode')
-        col = layout.column()
-        col.prop(context.object.active_material, 'tex_coord_mapper_0')
-        col = layout.column()
-        col.prop(context.object.active_material, 'tex_coord_mapper_1')
-        col = layout.column()
-        col.prop(context.object.active_material, 'tex_coord_transform_0')
-        col = layout.column()
-        col.prop(context.object.active_material, 'tex_coord_transform_1')
-        col = layout.column()
-        col.prop(context.object.active_material, 'environment_texture')
-        col = layout.column()
-        col.prop(context.object.active_material, 'environment_mult')
-        col = layout.column()
-        col.prop(context.object.active_material, 'recolor_texture')
-        col = layout.column()
-        col.prop(context.object.active_material, 'recolor_mult')
-        col = layout.column()
-        col.prop(context.object.active_material, 'use_recolor')
-        col = layout.column()
-        col.prop(context.object.active_material, 'house_color_pulse')
-        col = layout.column()
-        col.prop(context.object.active_material, 'scrolling_mask_texture')
-        col = layout.column()
-        col.prop(context.object.active_material, 'tex_coord_transform_angle')
-        col = layout.column()
-        col.prop(context.object.active_material, 'tex_coord_transform_u_0')
-        col = layout.column()
-        col.prop(context.object.active_material, 'tex_coord_transform_v_0')
-        col = layout.column()
-        col.prop(context.object.active_material, 'tex_coord_transform_u_1')
-        col = layout.column()
-        col.prop(context.object.active_material, 'tex_coord_transform_v_1')
-        col = layout.column()
-        col.prop(context.object.active_material, 'tex_coord_transform_u_2')
-        col = layout.column()
-        col.prop(context.object.active_material, 'tex_coord_transform_v_2')
-        col = layout.column()
-        col.prop(context.object.active_material, 'tex_ani_fps_NPR_lastFrame_frameOffset_0')
-
-
 ##########################################################################
 # Material.Shader
 ##########################################################################
@@ -456,49 +353,3 @@ class ShaderProperties(PropertyGroup):
     alpha_test: bpy.props.IntProperty(min=0, max=255)
     post_detail_color_func: bpy.props.IntProperty(min=0, max=255)
     post_detail_alpha_func: bpy.props.IntProperty(min=0, max=255)
-
-
-bpy.utils.register_class(ShaderProperties)
-Material.shader = PointerProperty(type=ShaderProperties)
-
-
-class MATERIAL_SHADER_PROPERTIES_PANEL_PT_w3d(Panel):
-    bl_label = 'W3D Shader Properties'
-    bl_space_type = 'PROPERTIES'
-    bl_region_type = 'WINDOW'
-    bl_context = 'material'
-
-    def draw(self, context):
-        layout = self.layout
-        col = layout.column()
-        col.prop(context.object.active_material.shader, 'depth_compare')
-        col = layout.column()
-        col.prop(context.object.active_material.shader, 'depth_mask')
-        col = layout.column()
-        col.prop(context.object.active_material.shader, 'color_mask')
-        col = layout.column()
-        col.prop(context.object.active_material.shader, 'dest_blend')
-        col = layout.column()
-        col.prop(context.object.active_material.shader, 'fog_func')
-        col = layout.column()
-        col.prop(context.object.active_material.shader, 'pri_gradient')
-        col = layout.column()
-        col.prop(context.object.active_material.shader, 'sec_gradient')
-        col = layout.column()
-        col.prop(context.object.active_material.shader, 'src_blend')
-        col = layout.column()
-        col.prop(context.object.active_material.shader, 'texturing')
-        col = layout.column()
-        col.prop(context.object.active_material.shader, 'detail_color_func')
-        col = layout.column()
-        col.prop(context.object.active_material.shader, 'detail_alpha_func')
-        col = layout.column()
-        col.prop(context.object.active_material.shader, 'shader_preset')
-        col = layout.column()
-        col.prop(context.object.active_material.shader, 'alpha_test')
-        col = layout.column()
-        col.prop(context.object.active_material.shader,
-                 'post_detail_color_func')
-        col = layout.column()
-        col.prop(context.object.active_material.shader,
-                 'post_detail_alpha_func')
