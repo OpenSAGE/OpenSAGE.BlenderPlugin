bc3f53d2e4512df65ac7f48d38300fedae473168
Michael Schnabel <michael.schnabel@exone.com>
2020-01-28 13:17:48 +0100

    got rigging tests working

--- tests/shared/cases/test_utils.py
+++ tests/shared/cases/test_utils.py
@@ -250,11 +250,7 @@ class TestUtils(TestCase):
 
         (actual_hiera, rig) = retrieve_hierarchy(self, 'containerName')
 
-        for i, pivot in enumerate(hierarchy.pivots):
-            print(pivot.name + ' -> ' + actual_hiera.pivots[i].name)
-
-        #self.compare_data([], None, hierarchy)
-        # self.assertTrue(False)
+        self.compare_data([], None, hierarchy)
 
     def test_too_many_hierarchies_roundtrip(self):
         hierarchy = get_hierarchy()
@@ -392,6 +388,11 @@ class TestUtils(TestCase):
                  self.outpath() + 'texture.dds')
 
         create_data(self, meshes, hlod, hierarchy)
+
+        (actual_hiera, rig) = retrieve_hierarchy(self, 'container_name')
+
+        for pivot in actual_hiera.pivots:
+            print(pivot.name)
 
         self.compare_data(meshes, hlod, hierarchy)
 
