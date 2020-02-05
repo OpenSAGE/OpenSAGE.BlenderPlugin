2a18ae79da1ae327407373a7a73ced8c24df35e0
Michael Schnabel <michael-schnabel@gmx.net>
2020-02-05 20:32:11 +0100

    split utils classes

---
+++ io_mesh_w3d/shared/utils/hierarchy_export.py
@@ -0,0 +1,123 @@
+# <pep8 compliant>
+# Written by Stephan Vedder and Michael Schnabel
+
+from io_mesh_w3d.shared.utils.basics import *
+from io_mesh_w3d.shared.structs.hierarchy import *
+
+
+pick_plane_names = ['PICK']
+
+def switch_to_pose(rig, pose):
+    if rig is not None:
+        rig.data.pose_position = pose
+        bpy.context.view_layer.update()
+
+
+def process_pivot(pivot, pivots, hierarchy):
+    hierarchy.pivots.append(pivot)
+    children = [child for child in pivots if child.parent_id == pivot.name]
+    parent_index = len(hierarchy.pivots) - 1
+
+    for child in children:
+        child.parent_id = parent_index
+        process_pivot(child, pivots, hierarchy)
+    pivot.processed = True
+
+
+def retrieve_hierarchy(context, container_name):
+    hierarchy = Hierarchy(
+        header=HierarchyHeader(),
+        pivots=[],
+        pivot_fixups=[])
+
+    root = HierarchyPivot(
+        name='ROOTTRANSFORM',
+        parentID=-1,
+        translation=Vector())
+    hierarchy.pivots.append(root)
+
+    rig = None
+    rigs = get_objects('ARMATURE')
+    pivots = []
+
+    if len(rigs) == 0:
+        hierarchy.header.name = container_name
+        hierarchy.header.center_pos = Vector()
+    elif len(rigs) == 1:
+        rig = rigs[0]
+
+        switch_to_pose(rig, 'REST')
+
+        root.translation = rig.location
+
+        hierarchy.header.name = rig.name
+        hierarchy.header.center_pos = rig.location
+
+        for bone in rig.pose.bones:
+            pivot = HierarchyPivot(
+                name=bone.name,
+                parent_id=0)
+
+            matrix = bone.matrix
+
+            if bone.parent is not None:
+                pivot.parent_id = bone.parent.name
+                matrix = bone.parent.matrix.inverted() @ matrix
+
+            (translation, rotation, _) = matrix.decompose()
+            pivot.translation = translation
+            pivot.rotation = rotation
+            eulers = rotation.to_euler()
+            pivot.euler_angles = Vector((eulers.x, eulers.y, eulers.z))
+
+            pivots.append(pivot)
+
+        switch_to_pose(rig, 'POSE')
+    else:
+        context.error('only one armature per scene allowed!')
+        return (None, None)
+
+    meshes = []
+    if rig is not None:
+        for coll in bpy.data.collections:
+            if rig.name in coll.objects:
+                meshes = get_objects('MESH', coll.objects)
+    else:
+        meshes = get_objects('MESH')
+
+    for mesh in list(reversed(meshes)):
+        if mesh.vertex_groups \
+                or mesh.object_type == 'BOX' \
+                or mesh.name in pick_plane_names:
+            continue
+
+        if mesh.delta_location.length < 0.01 \
+                and mesh.delta_rotation_quaternion == Quaternion():
+            continue
+
+        eulers = mesh.rotation_quaternion.to_euler()
+        pivot = HierarchyPivot(
+            name=mesh.name,
+            parent_id=0,
+            translation=mesh.delta_location,
+            rotation=mesh.delta_rotation_quaternion,
+            euler_angles=Vector((eulers.x, eulers.y, eulers.z)))
+
+        if mesh.parent_bone != '':
+            pivot.parent_id = mesh.parent_bone
+        elif mesh.parent is not None:
+            pivot.parent_id = mesh.parent.name
+
+        pivots.append(pivot)
+
+    for pivot in pivots:
+        pivot.processed = False
+
+    for pivot in pivots:
+        if pivot.processed or pivot.parent_id != 0:
+            continue
+        process_pivot(pivot, pivots, hierarchy)
+
+    hierarchy.header.num_pivots = len(hierarchy.pivots)
+    return (hierarchy, rig)
+
---
+++ io_mesh_w3d/shared/utils/primitives.py
@@ -0,0 +1,30 @@
+# <pep8 compliant>
+# Written by Stephan Vedder and Michael Schnabel
+
+import bpy
+import bmesh
+
+
+def create_sphere():
+    mesh = bpy.data.meshes.new('Basic_Sphere')
+    basic_sphere = bpy.data.objects.new("Basic_Sphere", mesh)
+
+    b_mesh = bmesh.new()
+    bmesh.ops.create_uvsphere(b_mesh, u_segments=12, v_segments=6, diameter=35)
+    b_mesh.to_mesh(mesh)
+    b_mesh.free()
+
+    return basic_sphere
+
+
+def create_cone(name):
+    mesh = bpy.data.meshes.new(name)
+    cone = bpy.data.objects.new(name, mesh)
+
+    b_mesh = bmesh.new()
+    bmesh.ops.create_cone(b_mesh, cap_ends=True, cap_tris=True,
+                          segments=10, diameter1=0, diameter2=1.0, depth=2.0, calc_uvs=True)
+    b_mesh.to_mesh(mesh)
+    b_mesh.free()
+
+    return (mesh, cone)
