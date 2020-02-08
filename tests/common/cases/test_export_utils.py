990958a9a7aa440a635234e907f28efb3308db25
Stephan Vedder <stephan.vedder@gmail.com>
2020-02-08 18:25:07 +0000

    autopep8 action fixes

--- tests/common/cases/test_export_utils.py
+++ tests/common/cases/test_export_utils.py
@@ -9,6 +9,7 @@ from tests.common.helpers.mesh import *
 from io_mesh_w3d.common.utils.hierarchy_export import retrieve_hierarchy
 from io_mesh_w3d.common.utils.mesh_export import retrieve_meshes
 
+
 class TestExportUtils(TestCase):
     def test_retrieve_data_returns_false_if_w3d_and_container_name_too_long(self):
         self.error = lambda text: self.assertEqual('Filename is longer than 16 characters, aborting export!', text)
@@ -26,11 +27,11 @@ class TestExportUtils(TestCase):
         export_settings = {}
         export_settings['mode'] = 'M'
 
-        self.filepath = 'C:dir\dir.dir\toolongtestfilename'
+        self.filepath = r'C:dir\dir.dir\toolongtestfilename'
 
         self.assertFalse(retrieve_data(self, export_settings, data_context))
 
-    #@patch('io_mesh_w3d.common.utils.hierarchy_export.retrieve_hierarchy', return_value=(get_hierarchy(), None))
-    #@patch('io_mesh_w3d.common.utils.mesh_export.retrieve_meshes', return_value=[get_mesh()])
-    #def test_retrieve_data_returns_false_if_meshes_are_invalid(self):
+    # @patch('io_mesh_w3d.common.utils.hierarchy_export.retrieve_hierarchy', return_value=(get_hierarchy(), None))
+    # @patch('io_mesh_w3d.common.utils.mesh_export.retrieve_meshes', return_value=[get_mesh()])
+    # def test_retrieve_data_returns_false_if_meshes_are_invalid(self):
     # TODO: find a way to mock function calls
