b89f0bf4942d2b1cdfd525c28e845a2dc23703c2
Stephan Vedder <stephan.vedder@gmail.com>
2020-02-09 21:25:19 +0000

    autopep8 action fixes

--- tests/common/cases/test_export_utils.py
+++ tests/common/cases/test_export_utils.py
@@ -9,6 +9,7 @@ from tests.common.helpers.mesh import *
 from tests.common.helpers.hlod import *
 from tests.common.helpers.collision_box import *
 
+
 class TestExportUtils(TestCase):
     def test_retrieve_data_returns_false_if_w3d_and_container_name_too_long(self):
         self.error = lambda text: self.assertEqual('Filename is longer than 16 characters, aborting export!', text)
@@ -35,7 +36,8 @@ class TestExportUtils(TestCase):
     @patch('io_mesh_w3d.export_utils.retrieve_boxes', return_value=[])
     @patch('io_mesh_w3d.export_utils.retrieve_dazzles', return_value=[])
     @patch('io_mesh_w3d.export_utils.retrieve_meshes', return_value=([], None))
-    def test_retrieve_data_returns_false_if_M_in_mode_and_no_meshes(self, retrieve_hierarchy, hlod, boxes, dazzles, retrieve_meshes):
+    def test_retrieve_data_returns_false_if_M_in_mode_and_no_meshes(
+            self, retrieve_hierarchy, hlod, boxes, dazzles, retrieve_meshes):
         self.error = lambda text: self.assertEqual('Scene does not contain any meshes, aborting export!', text)
 
         data_context = DataContext(
@@ -59,9 +61,10 @@ class TestExportUtils(TestCase):
     @patch('io_mesh_w3d.export_utils.create_hlod', return_value=None)
     @patch('io_mesh_w3d.export_utils.retrieve_boxes', return_value=[])
     @patch('io_mesh_w3d.export_utils.retrieve_dazzles', return_value=[])
-    @patch('io_mesh_w3d.export_utils.retrieve_meshes', return_value = ([get_mesh(), get_mesh()], None))
+    @patch('io_mesh_w3d.export_utils.retrieve_meshes', return_value=([get_mesh(), get_mesh()], None))
     @patch.object(Mesh, 'validate', return_value=False)
-    def test_retrieve_data_returns_false_if_M_in_mode_and_invalid_mesh(self, hiera, hlod, boxes, dazzles, retrieve_meshes, validate):
+    def test_retrieve_data_returns_false_if_M_in_mode_and_invalid_mesh(
+            self, hiera, hlod, boxes, dazzles, retrieve_meshes, validate):
         self.error = lambda text: self.assertEqual('aborting export!', text)
 
         data_context = DataContext(
@@ -87,9 +90,10 @@ class TestExportUtils(TestCase):
     @patch('io_mesh_w3d.export_utils.create_hlod', return_value=None)
     @patch('io_mesh_w3d.export_utils.retrieve_boxes', return_value=[])
     @patch('io_mesh_w3d.export_utils.retrieve_dazzles', return_value=[])
-    @patch('io_mesh_w3d.export_utils.retrieve_meshes', return_value = ([get_mesh(), get_mesh()], None))
+    @patch('io_mesh_w3d.export_utils.retrieve_meshes', return_value=([get_mesh(), get_mesh()], None))
     @patch.object(Hierarchy, 'validate', return_value=False)
-    def test_retrieve_data_returns_false_if_H_in_mode_and_invalid_hierarchy(self, hiera, hlod, boxes, dazzles, retrieve_meshes, validate):
+    def test_retrieve_data_returns_false_if_H_in_mode_and_invalid_hierarchy(
+            self, hiera, hlod, boxes, dazzles, retrieve_meshes, validate):
         self.error = lambda text: self.assertEqual('aborting export!', text)
 
         data_context = DataContext(
@@ -115,10 +119,11 @@ class TestExportUtils(TestCase):
     @patch('io_mesh_w3d.export_utils.create_hlod', return_value=get_hlod())
     @patch('io_mesh_w3d.export_utils.retrieve_boxes', return_value=[])
     @patch('io_mesh_w3d.export_utils.retrieve_dazzles', return_value=[])
-    @patch('io_mesh_w3d.export_utils.retrieve_meshes', return_value = ([get_mesh(), get_mesh()], None))
+    @patch('io_mesh_w3d.export_utils.retrieve_meshes', return_value=([get_mesh(), get_mesh()], None))
     @patch.object(Hierarchy, 'validate', return_value=True)
     @patch.object(HLod, 'validate', return_value=False)
-    def test_retrieve_data_returns_false_if_mode_is_HM_and_invalid_hlod(self, hiera, hlod, boxes, dazzles, retrieve_meshes, h_validate, validate):
+    def test_retrieve_data_returns_false_if_mode_is_HM_and_invalid_hlod(
+            self, hiera, hlod, boxes, dazzles, retrieve_meshes, h_validate, validate):
         self.error = lambda text: self.assertEqual('aborting export!', text)
 
         data_context = DataContext(
@@ -145,11 +150,12 @@ class TestExportUtils(TestCase):
     @patch('io_mesh_w3d.export_utils.create_hlod', return_value=get_hlod())
     @patch('io_mesh_w3d.export_utils.retrieve_boxes', return_value=[get_collision_box()])
     @patch('io_mesh_w3d.export_utils.retrieve_dazzles', return_value=[])
-    @patch('io_mesh_w3d.export_utils.retrieve_meshes', return_value = ([get_mesh(), get_mesh()], None))
+    @patch('io_mesh_w3d.export_utils.retrieve_meshes', return_value=([get_mesh(), get_mesh()], None))
     @patch.object(Hierarchy, 'validate', return_value=True)
     @patch.object(HLod, 'validate', return_value=True)
     @patch.object(CollisionBox, 'validate', return_value=False)
-    def test_retrieve_data_returns_false_if_mode_is_HM_and_invalid_box(self, hiera, hlod, boxes, dazzles, retrieve_meshes, h_validate, hlod_validate, validate):
+    def test_retrieve_data_returns_false_if_mode_is_HM_and_invalid_box(
+            self, hiera, hlod, boxes, dazzles, retrieve_meshes, h_validate, hlod_validate, validate):
         self.error = lambda text: self.assertEqual('aborting export!', text)
 
         data_context = DataContext(
@@ -172,5 +178,3 @@ class TestExportUtils(TestCase):
         h_validate.assert_called()
         hlod_validate.assert_called()
         validate.assert_called()
-
-  
