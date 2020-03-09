3773061cbaae8c58927a9cb24d6197ff7f2c39ab
Michael Schnabel <michael.schnabel@exone.com>
2020-03-09 10:14:07 +0100

    refactoring on rigging stuff

--- io_mesh_w3d/common/utils/helpers.py
+++ io_mesh_w3d/common/utils/helpers.py
@@ -67,28 +67,25 @@ def link_object_to_active_scene(obj, coll):
 
 
 def rig_object(obj, hierarchy, rig, sub_object):
+    obj.parent = rig
     pivot = hierarchy.pivots[sub_object.bone_index]
 
-    if pivot.name in rig.pose.bones:
-        obj.parent = rig
+    if pivot.name in rig.data.bones:
         obj.parent_bone = pivot.name
         obj.parent_type = 'BONE'
-    else:
-        obj.rotation_mode = 'QUATERNION'
-        obj.delta_location = pivot.translation
-        obj.delta_rotation_quaternion = pivot.rotation
+        return
+
+    obj.rotation_mode = 'QUATERNION'
+    obj.delta_location = pivot.translation
+    obj.delta_rotation_quaternion = pivot.rotation
 
     parent_pivot = hierarchy.pivots[pivot.parent_id]
 
     if parent_pivot.name in bpy.data.objects:
-        # we might have to reference by subobject name here
         obj.parent = bpy.data.objects[parent_pivot.name]
-    elif parent_pivot.name in rig.pose.bones:
-        obj.parent = rig
+    elif parent_pivot.name in rig.data.bones:
         obj.parent_bone = parent_pivot.name
         obj.parent_type = 'BONE'
-    else:
-        obj.parent = rig
 
 
 def create_uvlayer(context, mesh, b_mesh, tris, mat_pass):
