f5ec013a501c0729a13b6daa3f3aa9bb77c8f7a2
Michael Schnabel <michael-schnabel@gmx.net>
2020-04-12 12:44:39 +0200

    some small changes

--- io_mesh_w3d/common/node_groups/helpers.py
+++
@@ -1,8 +0,0 @@
-# <pep8 compliant>
-# Written by Stephan Vedder and Michael Schnabel
-
-def addInputInt(group, name, default=0, min=0, max=255):
-    group.inputs.new('NodeSocketInt', name)
-    group.inputs[name].default_value = default
-    group.inputs[name].min_value = min
-    group.inputs[name].max_value = max
