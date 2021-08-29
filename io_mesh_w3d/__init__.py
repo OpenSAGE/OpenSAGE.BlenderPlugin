24c65136508e0eb385a8ebec02cd17ed9524b242
Tarcontar <Tarcontar@users.noreply.github.com>
2021-08-29 19:01:39 +0000

    autopep8 action fixes

--- io_mesh_w3d/__init__.py
+++ io_mesh_w3d/__init__.py
@@ -445,7 +445,7 @@ class OBJECT_PT_DemoUpdaterPanel(bpy.types.Panel):
         col.label(text='popup triggered by opening')
         col.label(text='this panel, plus a box ui')
 
-        if addon_updater_ops.updater.update_ready == True:
+        if addon_updater_ops.updater.update_ready:
             layout.label(text='An update for the W3D/W3X plugin is available', icon='INFO')
         layout.label(text='')
 
@@ -460,34 +460,34 @@ class DemoPreferences(bpy.types.AddonPreferences):
         name='Auto-check for Update',
         description='If enabled, auto-check for updates using an interval',
         default=False,
-        )
+    )
     updater_intrval_months = bpy.props.IntProperty(
         name='Months',
         description='Number of months between checking for updates',
         default=0,
         min=0
-        )
+    )
     updater_intrval_days = bpy.props.IntProperty(
         name='Days',
         description='Number of days between checking for updates',
         default=7,
         min=0,
         max=31
-        )
+    )
     updater_intrval_hours = bpy.props.IntProperty(
         name='Hours',
         description='Number of hours between checking for updates',
         default=0,
         min=0,
         max=23
-        )
+    )
     updater_intrval_minutes = bpy.props.IntProperty(
         name='Minutes',
         description='Number of minutes between checking for updates',
         default=0,
         min=0,
         max=59
-        )
+    )
 
     def draw(self, context):
         layout = self.layout
